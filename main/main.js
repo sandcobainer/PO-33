2eb9d87ab67ebe20148166a03bc6da811922c60f
sandeep <sandeep.cr007@gmail.com>
2020-01-10 21:34:11 -0500

    Node server for PO33 guide

--- main/main.js
+++ main/main.js
@@ -1,9 +1,9 @@
 function loadApp() {
-    document.getElementById('app').innerHTML='<object type="text/html" data="./main/app.html"></object>';
+    document.getElementById('app').innerHTML='<object type="text/html" data="/app.html"></object>';
 }
 
 function loadTips() {
-    document.getElementById('tips').innerHTML='<object type="text/html" data="./main/tips.html"></object>';
+    document.getElementById('tips').innerHTML='<object type="text/html" data="/tips.html"></object>';
 }
 
 loadTips();
---
+++ start.js
@@ -0,0 +1,16 @@
+// server.js
+const express = require('express');
+const server = express();
+
+//setting middleware
+server.use(express.static(__dirname + '/main')); //Serves resources from public folder
+server.use(express.static(__dirname + '/style'));
+
+const port = 5000;
+server.listen(port, () => {
+    console.log(`Server listening at ${port}`);
+});
+
+server.get("/", (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+ });
